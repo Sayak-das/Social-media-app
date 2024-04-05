abc7ff04c29ec348e689e7a597b386dda5bf3b43
Sayak Das <dassayak14@gmail.com>
2024-04-05 16:58:13 +0530

    second commit

--- src/store/post-list-store.jsx
+++ src/store/post-list-store.jsx
@@ -1,13 +1,49 @@
-const { createContext, useReducer } = require("react");
+import { createContext, useReducer } from "react";
 
-const PostList = createContext({
-  postList : [],
-  addPost: ()=>{}
-  deletePost: ()=>{}
+const DEFAULT_CONTEXT = {};
+
+export const PostList = createContext({
+  postList: [],
+  addPost: () => {},
+  deletePost: () => {},
 });
+const addPost = () => {};
+const deletePost = () => {};
+
+const postListReducer = (currPostList, action) => {
+  return currPostList;
+};
 
 const PostListProvider = ({ children }) => {
-  const [postList, dispatchPostList] = useReducer();
-  return <PostList.Provider value={[]}>{children}</PostList.Provider>;
+  const [postList, dispatchPostList] = useReducer(
+    postListReducer,
+    DEFAULT_POST_LIST
+  );
+
+  return (
+    <PostList.Provider value={{ postList, addPost, deletePost }}>
+      {children}
+    </PostList.Provider>
+  );
 };
+
+const DEFAULT_POST_LIST = [
+  {
+    id: "1",
+    title: "Going to Mumbai",
+    body: "GOOOOINGGGGGGG BROOOO GOINGGGGG",
+    reactions: 2,
+    userId: "user-9",
+    tags: ["vacation", "mumbai"],
+  },
+  {
+    id: "2",
+    title: "Going to Mumbai",
+    body: "GOOOOINGGGGGGG BROOOO GOINGGGGG",
+    reactions: 5,
+    userId: "user-5",
+    tags: ["vacation", "mumbai", "russian"],
+  },
+];
+
 export default PostListProvider;
